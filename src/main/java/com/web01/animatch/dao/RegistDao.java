7052009afaea38a005d9ff102e445aaf8d1a8764
ShotaTsuji <ShotaTsuji@BT054FF>
2021-03-06 11:01:10 +0900

    新規登録機能追加レビュー Daoクラス #10

--- src/main/java/com/web01/animatch/dao/RegistDao.java
+++ src/main/java/com/web01/animatch/dao/RegistDao.java
@@ -36,22 +36,6 @@ public class RegistDao {
 
 	//定数
 	/**
-	 * ユーザテーブルカラム数
-	 */
-//	private static final int USER_COL_COUNT = 14;
-	/**
-	 * ペットテーブルカラム数
-	 */
-//	private static final int PET_COL_COUNT = 10;
-	/**
-	 * 店舗テーブルカラム数
-	 */
-//	private static final int STORE_COL_COUNT = 9;
-	/**
-	 * 営業時間テーブルカラム数
-	 */
-//	private static final int BUSINESS_HOURS_COL_COUNT = 8;
-	/**
 	 * IDデフォルト値
 	 */
 	private static final int DEFAULT_ID = 1000000000;
@@ -129,36 +113,6 @@ public class RegistDao {
 	public boolean registOwner(User user) throws SQLException {
 		Pet pet = user.getPet();
 
-//		List<Object> userDataList = new ArrayList<>();
-//		List<Object> petDataList = new ArrayList<>();
-//
-//		userDataList.add(user.getUserId());
-//		userDataList.add(user.getUserName());
-//		userDataList.add(user.getPassword());
-//		userDataList.add(user.getSex());
-//		userDataList.add(new Date(user.getBirthday().getTime()));
-//		userDataList.add(user.getPostalCode());
-//		userDataList.add(user.getStreetAddress());
-//		userDataList.add(user.getEmailAddress());
-//		userDataList.add(user.getTelephoneNumber());
-//		userDataList.add(pet.getPetId());
-//		userDataList.add(Integer.valueOf(DEFAULT_ID));
-//		userDataList.add(user.getIsDeleted());
-//		userDataList.add(Timestamp.valueOf(user.getInsertedTime()));
-//		userDataList.add(Timestamp.valueOf(user.getUpdatedTime()));
-//
-//		petDataList.add(pet.getPetId());
-//		petDataList.add(pet.getImage());
-//		petDataList.add(pet.getNickName());
-//		petDataList.add(pet.getSex());
-//		petDataList.add(pet.getType());
-//		petDataList.add(pet.getWeight());
-//		petDataList.add(pet.getRemarks());
-//		petDataList.add(pet.getIsDeleted());
-//		petDataList.add(Timestamp.valueOf(pet.getInsertedTime()));
-//		petDataList.add(Timestamp.valueOf(pet.getUpdatedTime()));
-
-
 		List<RegistData> userDataList = new ArrayList<>();
 		List<RegistData> petDataList = new ArrayList<>();
 
@@ -190,40 +144,6 @@ public class RegistDao {
 
 		this.con.setAutoCommit(false);
 
-//		String sql = "INSERT INTO t_user VALUES(";
-//		for(int i = 0; i < USER_COL_COUNT - 1; i++) {
-//			sql += "?, ";
-//		}
-//		sql += "?)";
-//		PreparedStatement pstmt = con.prepareStatement(sql);
-//
-//		setSQLIntegerParameter(pstmt, 1, user.getUserId());
-//		setSQLVarcharParameter(pstmt, 2, user.getUserName());
-//		setSQLVarcharParameter(pstmt, 3, user.getPassword());
-//		setSQLVarcharParameter(pstmt, 4, user.getSex());
-//		setSQLDateParameter(pstmt, 5, new Date(user.getBirthday().getTime()));
-//		setSQLVarcharParameter(pstmt, 6, user.getPostalCode());
-//		setSQLVarcharParameter(pstmt, 7, user.getStreetAddress());
-//		setSQLVarcharParameter(pstmt, 8, user.getEmailAddress());
-//		setSQLVarcharParameter(pstmt, 9, user.getTelephoneNumber());
-//		setSQLIntegerParameter(pstmt, 10, pet.getPetId());
-//		pstmt.setInt(11, DEFAULT_ID);
-//		pstmt.setInt(12, user.getIsDeleted());
-//		pstmt.setTimestamp(13, Timestamp.valueOf(user.getInsertedTime()));
-//		pstmt.setTimestamp(14, Timestamp.valueOf(user.getUpdatedTime()));
-
-//		int userDataParamCount = userDataList.size();
-//		String sql = "INSERT INTO t_user VALUES(";
-//		for(int i = 1; i < userDataParamCount; i++) {
-//			sql += "?, ";
-//		}
-//		sql += "?)";
-//		PreparedStatement pstmt = con.prepareStatement(sql);
-//
-//		for (int i = 0; i < userDataParamCount; i++) {
-//			setSqlParameter(pstmt, (i+1), userDataList.get(i));
-//		}
-
 		int userDataParamCount = userDataList.size();
 		String sql = "INSERT INTO t_user VALUES(";
 		for(int i = 1; i < userDataParamCount; i++) {
@@ -237,36 +157,6 @@ public class RegistDao {
 		}
 
 		pstmt.executeUpdate();
-
-//		sql = "INSERT INTO t_pet VALUES(";
-//		for(int i = 0; i < PET_COL_COUNT - 1; i++) {
-//			sql += "?, ";
-//		}
-//		sql += "?)";
-//		pstmt = con.prepareStatement(sql);
-//
-//		setSQLIntegerParameter(pstmt, 1, pet.getPetId());
-//		setSQLBlobParameter(pstmt, 2, pet.getImage());
-//		setSQLVarcharParameter(pstmt, 3, pet.getNickName());
-//		setSQLVarcharParameter(pstmt, 4, pet.getSex());
-//		setSQLVarcharParameter(pstmt, 5, pet.getType());
-//		setSQLFloatParameter(pstmt, 6, pet.getWeight());
-//		setSQLVarcharParameter(pstmt, 7, pet.getRemarks());
-//		pstmt.setInt(8, pet.getIsDeleted());
-//		pstmt.setTimestamp(9, Timestamp.valueOf(pet.getInsertedTime()));
-//		pstmt.setTimestamp(10, Timestamp.valueOf(pet.getUpdatedTime()));
-
-//		int petDataParamCount = petDataList.size();
-//		sql = "INSERT INTO t_pet VALUES(";
-//		for(int i = 1; i < petDataParamCount; i++) {
-//			sql += "?, ";
-//		}
-//		sql += "?)";
-//		pstmt = con.prepareStatement(sql);
-//
-//		for (int i = 0; i < petDataParamCount; i++) {
-//			setSqlParameter(pstmt, (i+1), petDataList.get(i));
-//		}
 
 		int petDataParamCount = petDataList.size();
 		sql = "INSERT INTO t_pet VALUES(";
@@ -296,51 +186,6 @@ public class RegistDao {
 	public boolean registTrimmer(User user) throws SQLException {
 		Store store = user.getStore();
 		List<BusinessHours> businessHoursList = user.getStore().getBusinessHoursList();
-
-//		List<Object> userDataList = new ArrayList<>();
-//		List<Object> storeDataList = new ArrayList<>();
-//		Map<Integer, List<Object>> businessHoursDataMap = new HashMap<>();
-//
-//		userDataList.add(user.getUserId());
-//		userDataList.add(user.getUserName());
-//		userDataList.add(user.getPassword());
-//		userDataList.add(user.getSex());
-//		userDataList.add(new Date(user.getBirthday().getTime()));
-//		userDataList.add(user.getPostalCode());
-//		userDataList.add(user.getStreetAddress());
-//		userDataList.add(user.getEmailAddress());
-//		userDataList.add(user.getTelephoneNumber());
-//		userDataList.add(Integer.valueOf(DEFAULT_ID));
-//		userDataList.add(store.getStoreId());
-//		userDataList.add(user.getIsDeleted());
-//		userDataList.add(Timestamp.valueOf(user.getInsertedTime()));
-//		userDataList.add(Timestamp.valueOf(user.getUpdatedTime()));
-//
-//		int storeId = store.getStoreId();
-//		storeDataList.add(storeId);
-//		storeDataList.add(store.getImage());
-//		storeDataList.add(store.getStoreName());
-//		storeDataList.add(store.getEmployeesNumber());
-//		storeDataList.add(store.getCourseInfo());
-//		storeDataList.add(store.getCommitment());
-//		storeDataList.add(store.getIsDeleted());
-//		storeDataList.add(Timestamp.valueOf(store.getInsertedTime()));
-//		storeDataList.add(Timestamp.valueOf(store.getUpdatedTime()));
-//
-//		if(businessHoursList != null) {
-//			for(int i = 0; i < businessHoursList.size(); i++) {
-//				List<Object> businessHoursDataList = new ArrayList<>();
-//				businessHoursDataList.add(storeId);
-//				businessHoursDataList.add(businessHoursList.get(i).getBusinessDay());
-//				businessHoursDataList.add(Time.valueOf(businessHoursList.get(i).getStartBusinessTime()));
-//				businessHoursDataList.add(Time.valueOf(businessHoursList.get(i).getEndBusinessTime()));
-//				businessHoursDataList.add(businessHoursList.get(i).getComplement());
-//				businessHoursDataList.add(businessHoursList.get(i).getIsDeleted());
-//				businessHoursDataList.add(Timestamp.valueOf(businessHoursList.get(i).getInsertedTime()));
-//				businessHoursDataList.add(Timestamp.valueOf(businessHoursList.get(i).getUpdatedTime()));
-//				businessHoursDataMap.put(i, businessHoursDataList);
-//			}
-//		}
 
 		List<RegistData> userDataList = new ArrayList<>();
 		List<RegistData> storeDataList = new ArrayList<>();
@@ -389,40 +234,6 @@ public class RegistDao {
 
 		this.con.setAutoCommit(false);
 
-//		String sql = "INSERT INTO t_user VALUES(";
-//		for(int i = 0; i < USER_COL_COUNT - 1; i++) {
-//			sql += "?, ";
-//		}
-//		sql += "?)";
-//		PreparedStatement pstmt = con.prepareStatement(sql);
-//
-//		setSQLIntegerParameter(pstmt, 1, user.getUserId());
-//		setSQLVarcharParameter(pstmt, 2, user.getUserName());
-//		setSQLVarcharParameter(pstmt, 3, user.getPassword());
-//		setSQLVarcharParameter(pstmt, 4, user.getSex());
-//		setSQLDateParameter(pstmt, 5, new Date(user.getBirthday().getTime()));
-//		setSQLVarcharParameter(pstmt, 6, user.getPostalCode());
-//		setSQLVarcharParameter(pstmt, 7, user.getStreetAddress());
-//		setSQLVarcharParameter(pstmt, 8, user.getEmailAddress());
-//		setSQLVarcharParameter(pstmt, 9, user.getTelephoneNumber());
-//		pstmt.setInt(10, DEFAULT_ID);
-//		setSQLIntegerParameter(pstmt, 11, store.getStoreId());
-//		pstmt.setInt(12, user.getIsDeleted());
-//		pstmt.setTimestamp(13, Timestamp.valueOf(user.getInsertedTime()));
-//		pstmt.setTimestamp(14, Timestamp.valueOf(user.getUpdatedTime()));
-
-//		int userDataParamCount = userDataList.size();
-//		String sql = "INSERT INTO t_user VALUES(";
-//		for(int i = 1; i < userDataParamCount; i++) {
-//			sql += "?, ";
-//		}
-//		sql += "?)";
-//		PreparedStatement pstmt = con.prepareStatement(sql);
-//
-//		for (int i = 0; i < userDataParamCount; i++) {
-//			setSqlParameter(pstmt, (i+1), userDataList.get(i));
-//		}
-
 		int userDataParamCount = userDataList.size();
 		String sql = "INSERT INTO t_user VALUES(";
 		for(int i = 1; i < userDataParamCount; i++) {
@@ -437,36 +248,6 @@ public class RegistDao {
 
 		pstmt.executeUpdate();
 
-//		sql = "INSERT INTO t_store VALUES(";
-//		for(int i = 0; i < STORE_COL_COUNT - 1; i++) {
-//			sql += "?, ";
-//		}
-//		sql += "?)";
-//		pstmt = con.prepareStatement(sql);
-//
-//		int storeId = store.getStoreId();
-//		setSQLIntegerParameter(pstmt, 1, storeId);
-//		setSQLBlobParameter(pstmt, 2, store.getImage());
-//		setSQLVarcharParameter(pstmt, 3, store.getStoreName());
-//		setSQLIntegerParameter(pstmt, 4, store.getEmployeesNumber());
-//		setSQLVarcharParameter(pstmt, 5, store.getCourseInfo());
-//		setSQLVarcharParameter(pstmt, 6, store.getCommitment());
-//		pstmt.setInt(7, store.getIsDeleted());
-//		pstmt.setTimestamp(8, Timestamp.valueOf(store.getInsertedTime()));
-//		pstmt.setTimestamp(9, Timestamp.valueOf(store.getUpdatedTime()));
-
-//		int storeDataParamCount = storeDataList.size();
-//		sql = "INSERT INTO t_store VALUES(";
-//		for(int i = 1; i < storeDataParamCount; i++) {
-//			sql += "?, ";
-//		}
-//		sql += "?)";
-//		pstmt = con.prepareStatement(sql);
-//
-//		for (int i = 0; i < storeDataParamCount; i++) {
-//			setSqlParameter(pstmt, (i+1), storeDataList.get(i));
-//		}
-
 		int storeDataParamCount = storeDataList.size();
 		sql = "INSERT INTO t_store VALUES(";
 		for(int i = 1; i < storeDataParamCount; i++) {
@@ -480,47 +261,6 @@ public class RegistDao {
 		}
 
 		pstmt.executeUpdate();
-
-//		if(businessHoursList != null) {
-//			for(int i = 0; i < businessHoursList.size(); i++) {
-//				sql = "INSERT INTO t_business_hours VALUES(";
-//				for(int j = 0; j < BUSINESS_HOURS_COL_COUNT - 1; j++) {
-//					sql += "?, ";
-//				}
-//				sql += "?)";
-//				pstmt = con.prepareStatement(sql);
-//
-//				setSQLIntegerParameter(pstmt, 1, storeId);
-//				setSQLVarcharParameter(pstmt, 2, businessHoursList.get(i).getBusinessDay());
-//				setSQLTimeParameter(pstmt, 3, Time.valueOf(businessHoursList.get(i).getStartBusinessTime()));
-//				setSQLTimeParameter(pstmt, 4, Time.valueOf(businessHoursList.get(i).getEndBusinessTime()));
-//				setSQLVarcharParameter(pstmt, 5, businessHoursList.get(i).getComplement());
-//				pstmt.setInt(6, businessHoursList.get(i).getIsDeleted());
-//				pstmt.setTimestamp(7, Timestamp.valueOf(businessHoursList.get(i).getInsertedTime()));
-//				pstmt.setTimestamp(8, Timestamp.valueOf(businessHoursList.get(i).getUpdatedTime()));
-//
-//				pstmt.executeUpdate();
-//			}
-//		}
-
-//		for(int i = 0; i < businessHoursDataMap.size(); i++) {
-//			List<Object> businessHoursDataList = businessHoursDataMap.get(i);
-//			int businessHoursDataParamCount = businessHoursDataList.size();
-//
-//			sql = "INSERT INTO t_business_hours VALUES(";
-//			for(int j = 1; j < businessHoursDataParamCount; j++) {
-//				sql += "?, ";
-//			}
-//			sql += "?)";
-//			pstmt = con.prepareStatement(sql);
-//
-//
-//			for (int k = 0; k < businessHoursDataParamCount; k++) {
-//				setSqlParameter(pstmt, (i+1), businessHoursDataList.get(k));
-//			}
-//
-//			pstmt.executeUpdate();
-//		}
 
 		for(int i = 0; i < businessHoursDataMap.size(); i++) {
 			List<RegistData> businessHoursDataList = businessHoursDataMap.get(i);
@@ -546,30 +286,6 @@ public class RegistDao {
 
 		return true;
 	}
-
-	/**
-	 * SQLパラメータ設定
-	 * @param pstmt プリコンパイルされたSQL文オブジェクト
-	 * @param paramIndex 引数インデックス
-	 * @param value 値
-	 * @return SQLパラメータに成功した場合、true 失敗した場合、false
-	 */
-//	private void setSqlParameter(PreparedStatement pstmt, int paramIndex, Object value) throws SQLException {
-//		String valStr = String.valueOf(value);
-//		if(value instanceof Integer) {
-//			setSQLIntegerParameter(pstmt, paramIndex, Integer.valueOf(valStr));
-//		} else if(value instanceof Float) {
-//			setSQLFloatParameter(pstmt, paramIndex, Float.valueOf(valStr));
-//		} else if(value instanceof String) {
-//			setSQLVarcharParameter(pstmt, paramIndex, valStr);
-//		} else if(value instanceof Date) {
-//			setSQLDateParameter(pstmt, paramIndex, Date.valueOf(valStr));
-//		} else if(value instanceof Time) {
-//			setSQLTimeParameter(pstmt, paramIndex, Time.valueOf(valStr));
-//		} else if(value instanceof Timestamp) {
-//			pstmt.setTimestamp(paramIndex, Timestamp.valueOf(valStr));
-//		}
-//	}
 
 	/**
 	 * SQLパラメータ設定
@@ -620,88 +336,4 @@ public class RegistDao {
 			pstmt.setTimestamp(paramIndex, Timestamp.valueOf(valStr));
 		}
 	}
-
-	/**
-	 * SQL数値パラメータ設定
-	 * @param pstmt プリコンパイルされたSQL文オブジェクト
-	 * @param paramIndex 引数インデックス
-	 * @param value 値
-	 */
-//	private void setSQLIntegerParameter(PreparedStatement pstmt, int paramIndex, Integer value) throws SQLException {
-//		if(value == null) {
-//			pstmt.setNull(paramIndex, Types.INTEGER);
-//		}else {
-//			pstmt.setInt(paramIndex, value);
-//		}
-//	}
-
-	/**
-	 * SQL文字列パラメータ設定
-	 * @param pstmt プリコンパイルされたSQL文オブジェクト
-	 * @param paramIndex 引数インデックス
-	 * @param value 値
-	 */
-//	private void setSQLVarcharParameter(PreparedStatement pstmt, int paramIndex, String value) throws SQLException {
-//		if(value == null || value.length() == 0) {
-//			pstmt.setNull(paramIndex, Types.VARCHAR);
-//		}else {
-//			pstmt.setString(paramIndex, value);
-//		}
-//	}
-
-	/**
-	 * SQL日付パラメータ設定
-	 * @param pstmt プリコンパイルされたSQL文オブジェクト
-	 * @param paramIndex 引数インデックス
-	 * @param value 値
-	 */
-//	private void setSQLDateParameter(PreparedStatement pstmt, int paramIndex, Date value) throws SQLException {
-//		if(value == null) {
-//			pstmt.setNull(paramIndex, Types.DATE);
-//		}else {
-//			pstmt.setDate(paramIndex, value);
-//		}
-//	}
-
-	/**
-	 * SQL小数パラメータ設定
-	 * @param pstmt プリコンパイルされたSQL文オブジェクト
-	 * @param paramIndex 引数インデックス
-	 * @param value 値
-	 */
-//	private void setSQLFloatParameter(PreparedStatement pstmt, int paramIndex, Float value) throws SQLException {
-//		if(value == null) {
-//			pstmt.setNull(paramIndex, Types.FLOAT);
-//		}else {
-//			pstmt.setFloat(paramIndex, value);
-//		}
-//	}
-
-	/**
-	 * SQLバイナリーオブジェクトパラメータ設定
-	 * @param pstmt プリコンパイルされたSQL文オブジェクト
-	 * @param paramIndex 引数インデックス
-	 * @param value 値
-	 */
-//	private void setSQLBlobParameter(PreparedStatement pstmt, int paramIndex, byte[] value) throws SQLException {
-//		if(value == null) {
-//			pstmt.setNull(paramIndex, Types.BLOB);
-//		}else {
-//			pstmt.setBinaryStream(paramIndex, new ByteArrayInputStream(value));
-//		}
-//	}
-
-	/**
-	 * SQL時間パラメータ設定
-	 * @param pstmt プリコンパイルされたSQL文オブジェクト
-	 * @param paramIndex 引数インデックス
-	 * @param value 値
-	 */
-//	private void setSQLTimeParameter(PreparedStatement pstmt, int paramIndex, Time value) throws SQLException {
-//		if(value == null) {
-//			pstmt.setNull(paramIndex, Types.TIME);
-//		}else {
-//			pstmt.setTime(paramIndex, value);
-//		}
-//	}
 }
