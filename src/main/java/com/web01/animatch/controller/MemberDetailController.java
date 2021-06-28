6fd41ddf183e861f2c36382631e147b4063c0475
tsusho <s.tuji3007sho@gmail.com>
2021-06-28 20:44:22 +0900

    会員詳細機能追加#37

---
+++ src/main/java/com/web01/animatch/controller/MemberDetailController.java
@@ -0,0 +1,52 @@
+package com.web01.animatch.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.web01.animatch.service.DetailService;
+
+/**
+ * MemberSearchDetailControllerクラス
+ * @author Tsuji
+ * @version 1.0
+ */
+public class MemberDetailController extends HttpServlet{
+
+	//定数
+	/**
+	 * シリアライズバージョンID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * デフォルトコンストラクタ
+	 */
+    public MemberDetailController() {
+        super();
+    }
+
+    /**
+	 * get送信
+	 * @param request リクエストオブジェクト
+	 * @param response レスポンスオブジェクト
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String reqURL = request.getRequestURI();
+		String userId = reqURL.substring(reqURL.lastIndexOf("/") + 1, reqURL.length());
+		if(StringUtils.isNumeric(userId)) {
+			String tmpReqURL = reqURL.substring(0, reqURL.lastIndexOf("/"));
+			DetailService detailService = new DetailService(tmpReqURL.substring(tmpReqURL.lastIndexOf("/") + 1, tmpReqURL.length()));
+			detailService.setDisplayAttribute(request, userId);
+			String path = "/WEB-INF/jsp/member/detail/detail.jsp";
+			RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+			dispatcher.forward(request, response);
+		}
+	}
+}
---
+++ src/main/java/com/web01/animatch/service/DetailService.java
@@ -0,0 +1,135 @@
+package com.web01.animatch.service;
+
+import java.sql.SQLException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.web01.animatch.dao.DBConnection;
+import com.web01.animatch.dao.ReadDao;
+import com.web01.animatch.dto.OwnerInfo;
+import com.web01.animatch.dto.TrimmerInfo;
+import com.web01.animatch.dto.TrimmerInfoBusinessHours;
+
+/**
+ * 詳細サービスクラス
+ * @author Tsuji
+ * @version 1.0
+ */
+public class DetailService extends BaseService {
+
+	//メンバー
+	/**
+	 * 検索区分
+	 */
+	private UserType searchDetailType;
+	/**
+	 * プロパティーサービスオブジェクト
+	 */
+	private PropertiesService propertiesService;
+
+	/**
+	 * 引数付きコンストラクタ
+	 * @param searchDetailType 検索詳細区分
+	 */
+	public DetailService(String searchDetailType) {
+		this.propertiesService = new PropertiesService();
+		switch(UserType.getEnumFromEnumName(searchDetailType.toUpperCase())) {
+			case OWNER:
+				this.searchDetailType = UserType.OWNER;
+				break;
+			case TRIMMER:
+				this.searchDetailType = UserType.TRIMMER;
+				break;
+			default:
+				break;
+		}
+	}
+
+	/**
+	 * 検索詳細データ設定
+	 * @param request リクエストオブジェクト
+	 * @param userId ユーザID
+	 */
+	private void setSearchDetailData(HttpServletRequest request, String userId) {
+		DBConnection con = new DBConnection();
+		ReadDao readDao = new ReadDao(con.getConnection());
+		try {
+			switch(this.searchDetailType) {
+				//飼い主の場合
+				case OWNER:
+					TrimmerInfo trimmerInfo = new TrimmerInfo();
+					if(StringUtils.isNumeric(userId)) {
+						trimmerInfo = readDao.findTrimmerInfoByUserId(Integer.parseInt(userId));
+
+						//画像をBase64化
+						trimmerInfo.setStoreImageBase64(convertByteAryToBase64(trimmerInfo.getStoreImage()));
+
+						//表示する営業時間文字列作成
+						List<TrimmerInfoBusinessHours> trimmerInfoBusinessHoursList = trimmerInfo.getTrimmerInfoBusinessHoursList();
+						for(TrimmerInfoBusinessHours trimmerInfoBusinessHours:trimmerInfoBusinessHoursList) {
+							//曜日、時間が設定されているデータのみ文字列結合
+							String businessDay = trimmerInfoBusinessHours.getBusinessDay();
+							LocalTime startBusinessTime = trimmerInfoBusinessHours.getStartBusinessTime();
+							LocalTime endBusinessTime = trimmerInfoBusinessHours.getEndBusinessTime();
+							if(!StringUtils.isEmpty(businessDay) && startBusinessTime != null && endBusinessTime != null) {
+								trimmerInfoBusinessHours.setDisplayBusinessHours(this.propertiesService.getValue(PropertiesService.WEEKDAY_KEY_INIT_STR + businessDay));
+								trimmerInfoBusinessHours.setDisplayStartBusinessTime(startBusinessTime.format(DateTimeFormatter.ofPattern("HH:mm")));
+								trimmerInfoBusinessHours.setDisplayEndBusinessTime(endBusinessTime.format(DateTimeFormatter.ofPattern("HH:mm")));
+							}
+						}
+					}
+
+					request.setAttribute("trimmerInfo", trimmerInfo);
+					break;
+				//トリマーの場合
+				case TRIMMER:
+					OwnerInfo ownerInfo = new OwnerInfo();
+					if(StringUtils.isNumeric(userId)) {
+						ownerInfo = readDao.findOwnerInfoByUserId(Integer.parseInt(userId));
+
+						//画像をBase64化
+						ownerInfo.setPetImageBase64(convertByteAryToBase64(ownerInfo.getPetImage()));
+						//会員検索画面表示文字セット
+						ownerInfo.setPetType(this.propertiesService.getValue(PropertiesService.PET_TYPE_KEY_INIT_STR + ownerInfo.getPetType()));
+						ownerInfo.setPetSex(this.propertiesService.getValue(PropertiesService.PET_SEX_KEY_INIT_STR + ownerInfo.getPetSex()));
+					}
+
+					request.setAttribute("ownerInfo", ownerInfo);
+					break;
+				default:
+					break;
+			}
+
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			con.close();
+		}
+	}
+
+	/**
+	 * 画面属性設定
+	 * @param request リクエストオブジェクト
+	 * @param userId ユーザID
+	 */
+	public void setDisplayAttribute(HttpServletRequest request, String userId) {
+		String searchDetailTypeId = null;
+		switch(this.searchDetailType) {
+		case OWNER:
+			searchDetailTypeId = UserType.OWNER.getId();
+			break;
+		case TRIMMER:
+			searchDetailTypeId = UserType.TRIMMER.getId();
+			break;
+		default:
+			break;
+		}
+		request.setAttribute("searchDetailTypeId", searchDetailTypeId);
+		setSearchDetailData(request, userId);
+	}
+}
