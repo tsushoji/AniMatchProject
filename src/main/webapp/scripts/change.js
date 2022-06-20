6e11bd745a247d9f98280be7765316d5ae16b218
tsusho <s.tuji3007sho@gmail.com>
2022-06-20 20:33:38 +0900

    アカウント設定機能追加 #48

--- src/main/webapp/scripts/change.js
+++ src/main/webapp/scripts/change.js
@@ -1,3 +1,94 @@
+const setChangeItemAttributeByInputOrSelect = function(targetEle, registedVal, className){
+ if(typeof registedVal === "undefined"){
+  registedVal = '';
+ }
+ let tarVal = targetEle.val();
+ //入力値が変更されたとき、クラス属性付与
+ if(tarVal === registedVal){
+  targetEle.removeClass(className);
+ }else{
+  targetEle.addClass(className);
+ }
+}
+
+const setChangeItemAttributeByInputPassword = function(targetEle, registedVal, className){
+ if(typeof registedVal === "undefined"){
+  registedVal = '';
+ }
+ let tarVal = targetEle.val();
+ //入力値が変更されたとき、クラス属性を付与
+ //パスワード表示要素も変更
+ let tarTextEle = targetEle.next();
+ if(typeof tarTextEle === "undefined"){
+  tarTextEle = targetEle.prev();
+ }
+ if(tarVal === registedVal){
+  targetEle.removeClass(className);
+  tarTextEle.removeClass(className);
+ }else{
+  targetEle.addClass(className);
+  tarTextEle.addClass(className);
+ }
+}
+
+const setChangeItemCssByWeekday = function(targetEle, registedIndexVal, attributeName, attributeValue){
+ let classNameAry = targetEle.attr('class').split(' ');
+ let targetEleIndex = targetEle.index();
+ let isOn = false;
+ if($.inArray('active',classNameAry)){
+  isOn = true;
+ }
+ let registedIndexAry = [-1];
+ if(typeof registedIndexVal !== "undefined"){
+  let tempRegistedIndexAry = registedIndexVal.split(',');
+  // 配列要素の文字列を数値に変換
+  let tempWeekdayAry =['0','1','2','3','4','5','6'];
+  let registedIndexAryIndex = 0;
+  if(isOn){
+   registedIndexAry = new Array(7-(tempRegistedIndexAry.length));
+   $.each(tempWeekdayAry, function(index, val) {
+    if($.inArray(val,tempRegistedIndexAry) === -1){
+     registedIndexAry[registedIndexAryIndex] = Number(val);
+     registedIndexAryIndex++;
+    }
+   });
+  }else{
+   registedIndexAry = new Array(tempRegistedIndexAry.length);
+   $.each(tempWeekdayAry, function(index, val) {
+    if($.inArray(val,tempRegistedIndexAry) !== -1){
+     registedIndexAry[registedIndexAryIndex] = Number(val);
+     registedIndexAryIndex++;
+    }
+   });
+  }
+ }
+
+ //multipickerにより、class属性が削除されてしまうため、style属性で代用
+ if($.inArray(targetEleIndex,registedIndexAry) !== -1){
+  targetEle.css(attributeName,'');
+ }else{
+  targetEle.css(attributeName,attributeValue);
+ }
+}
+
+const setChangeItemAttributeByUserSex = function(targetEle, registedVal, radioInputName, className){
+ let tarForName = targetEle.attr('id');
+ let changeEle = $('label[for="' + tarForName + '"]');
+ //入力値が変更されたとき、クラス属性を付与
+ if(tarVal !== registedVal){
+  changeEle.addClass(className);
+ }
+ $(radioInputName).each(function(i){
+  let tarEle = $(this);
+  let isChecked = tarEle.prop('checked');
+  if(!isChecked){
+   tarForName = tarEle.attr('id');
+   changeEle = $('label[for="' + tarForName + '"]');
+   changeEle.removeClass(className);
+  }
+ });
+}
+
 $(document).ready(function(){
  //初期表示時、フォームを非表示
  $('.form-common,.form-owner,.form-trimmer,.form-trimmer-business-hours,.footer-top-content').hide();
@@ -69,21 +160,10 @@ $(document).ready(function(){
   let targetEle = $(this);
 
   //入力値を取得
-  let tagName = targetEle[0].tagName;
-  let inputType = targetEle.attr('type');
   let registedId = registedIdPrefix;
-  let tarVal = targetEle.val();
   registedId += targetEle.attr('id');
   let registedVal = $(registedId).val();
-  if(typeof registedVal === "undefined"){
-   registedVal = '';
-  }
-  //入力値が変更されたとき、入力欄を赤色にする
-  if(tarVal === registedVal){
-   targetEle.removeClass(redBackGroundClassName);
-  }else{
-   targetEle.addClass(redBackGroundClassName);
-  }
+  setChangeItemAttributeByInputOrSelect(targetEle, registedVal, redBackGroundClassName);
  });
 
  //アクション:入力値(パスワード)の値が変更される
@@ -91,72 +171,18 @@ $(document).ready(function(){
   let targetEle = $(this);
 
   //入力値を取得
-  let tagName = targetEle[0].tagName;
-  let inputType = targetEle.attr('type');
   let registedId = registedIdPrefix;
-  let tarVal = targetEle.val();
   // パスワード表示ボタン押下対策
   registedId += 'password';
   let registedVal = $(registedId).val();
-  if(typeof registedVal === "undefined"){
-   registedVal = '';
-  }
-  //入力値が変更されたとき、入力欄を赤色にする
-  //パスワード表示要素も変更
-  let tarTextEle = targetEle.next();
-  if(typeof tarTextEle === "undefined"){
-   tarTextEle = targetEle.prev();
-  }
-  if(tarVal === registedVal){
-   targetEle.removeClass(redBackGroundClassName);
-   tarTextEle.removeClass(redBackGroundClassName);
-  }else{
-   targetEle.addClass(redBackGroundClassName);
-   tarTextEle.addClass(redBackGroundClassName);
-  }
+  setChangeItemAttributeByInputPassword(targetEle,registedVal,redBackGroundClassName);
  });
 
  //アクション:営業曜日入力値の値が変更される
  $('#check-changed-weekday-0, #check-changed-weekday-1, #check-changed-weekday-2, #check-changed-weekday-3, #check-changed-weekday-4, #check-changed-weekday-5, #check-changed-weekday-6').click(function() {
   let targetEle = $(this);
-  let classNameAry = targetEle.attr('class').split(' ');
-  let targetEleIndex = targetEle.index();
-  let isOn = false;
-  if($.inArray('active',classNameAry)){
-   isOn = true;
-  }
   let registedIndexVal = $(registedIdPrefix + 'form-business-hours').val();
-  let registedIndexAry = [-1];
-  if(typeof registedIndexVal !== "undefined"){
-   let tempRegistedIndexAry = registedIndexVal.split(',');
-   // 配列要素の文字列を数値に変換
-   let tempWeekdayAry =['0','1','2','3','4','5','6'];
-   let registedIndexAryIndex = 0;
-   if(isOn){
-    registedIndexAry = new Array(7-(tempRegistedIndexAry.length));
-    $.each(tempWeekdayAry, function(index, val) {
-     if($.inArray(val,tempRegistedIndexAry) === -1){
-      registedIndexAry[registedIndexAryIndex] = Number(val);
-      registedIndexAryIndex++;
-     }
-    });
-   }else{
-    registedIndexAry = new Array(tempRegistedIndexAry.length);
-    $.each(tempWeekdayAry, function(index, val) {
-     if($.inArray(val,tempRegistedIndexAry) !== -1){
-      registedIndexAry[registedIndexAryIndex] = Number(val);
-      registedIndexAryIndex++;
-     }
-    });
-   }
-  }
-
-  //multipickerにより、class属性が削除されてしまうため、style属性で代用
-  if($.inArray(targetEleIndex,registedIndexAry) !== -1){
-   targetEle.css('background-color','');
-  }else{
-   targetEle.css('background-color','#ffc107');
-  }
+  setChangeItemCssByWeekday(targetEle,registedIndexVal,'background-color','#ffc107');
  });
 
  //アクション:性別が変更される
@@ -165,24 +191,10 @@ $(document).ready(function(){
 
   //入力値を取得
   let registedId = registedIdPrefix;
-  let tarVal = targetEle.val();
   registedId += targetEle.attr('name');
   let registedVal = $(registedId).val();
-  let tarForName = targetEle.attr('id');
-  let changeEle = $('label[for="' + tarForName + '"]');
-  //入力値が変更されたとき、入力欄を赤色にする
-  if(tarVal !== registedVal){
-   changeEle.addClass(redBackGroundClassName);
-  }
-  $('form.account-change-form input[name="radio-user-sex"]').each(function(i){
-   let tarEle = $(this);
-   let isChecked = tarEle.prop('checked');
-   if(!isChecked){
-    tarForName = tarEle.attr('id');
-    changeEle = $('label[for="' + tarForName + '"]');
-    changeEle.removeClass(redBackGroundClassName);
-   }
-  });
+  let userSexRadioInputName = 'form.account-change-form input[name="radio-user-sex"]';
+  setChangeItemAttributeByUserSex(targetEle,registedVal,userSexRadioInputName,redBackGroundClassName);
  });
 
  //アクション:ペット性別が変更される
@@ -191,24 +203,10 @@ $(document).ready(function(){
 
   //入力値を取得
   let registedId = registedIdPrefix;
-  let tarVal = targetEle.val();
   registedId += targetEle.attr('name');
   let registedVal = $(registedId).val();
-  let tarForName = targetEle.attr('id');
-  let changeEle = $('label[for="' + tarForName + '"]');
-  //入力値が変更されたとき、入力欄を赤色にする
-  if(tarVal !== registedVal){
-   changeEle.addClass(redBackGroundClassName);
-  }
-  $('form.account-change-form input[name="radio-pet-sex"]').each(function(i){
-   let tarEle = $(this);
-   let isChecked = tarEle.prop('checked');
-   if(!isChecked){
-    tarForName = tarEle.attr('id');
-    changeEle = $('label[for="' + tarForName + '"]');
-    changeEle.removeClass(redBackGroundClassName);
-   }
-  });
+  let petSexRadioInputName = 'form.account-change-form input[name="radio-pet-sex"]';
+  setChangeItemAttributeByUserSex(targetEle,registedVal,petSexRadioInputName,redBackGroundClassName);
  });
 
  let formRegistType = $('#formRegistType').val();
