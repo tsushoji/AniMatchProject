e67e61f0034c69b5baf7eb46b6d509cbad083eb1
tsusho <s.tuji3007sho@gmail.com>
2022-07-02 21:55:00 +0900

    共通メンバーを親クラスで持つように修正 #48

--- src/main/java/com/web01/animatch/dto/AccountChangeForm.java
+++ src/main/java/com/web01/animatch/dto/AccountChangeForm.java
@@ -1,8 +1,8 @@
 package com.web01.animatch.dto;
 
-import java.util.List;
-
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 /**
  * アカウント変更フォームDtoクラス
@@ -10,87 +10,6 @@ import lombok.Data;
  * @version 1.0
  */
 @Data
-public class AccountChangeForm {
-
- //メンバー
- /**
-  * ユーザネーム
-  */
- private String userName;
- /**
-  * パスワード
-  */
- private String password;
- /**
-  * 性別
-  */
- private String sex;
- /**
-  * 誕生日
-  */
- private String birthday;
- /**
-  * 郵便番号
-  */
- private String postalCode;
- /**
-  * 都道府県
-  */
- private String prefectures;
- /**
-  * 市区町村
-  */
- private String cities;
- /**
-  * メールアドレス
-  */
- private String emailAddress;
- /**
-  * 電話番号
-  */
- private String telephoneNumber;
- /**
-  * ペットネーム
-  */
- private String petName;
- /**
-  * ペット性別
-  */
- private String petSex;
- /**
-  * ペット種類
-  */
- private String petType;
- /**
-  * ペット体重
-  */
- private String petWeight;
- /**
-  * 店名
-  */
- private String storeName;
- /**
-  * 営業時間インプットフォームValue属性
-  */
- private String formBusinessHoursInputValue;
- /**
-  * 営業時間フォームリスト
-  */
- private List<FormBusinessHours> formBusinessHoursList;
- /**
-  * 従業員数
-  */
- private String storeEmployees;
- /**
-  * ペット補足
-  */
- private String petRemarks;
- /**
-  * コース情報
-  */
- private String courseInfo;
- /**
-  * こだわり
-  */
- private String commitment;
-}
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class AccountChangeForm extends BaseAccountForm{}
